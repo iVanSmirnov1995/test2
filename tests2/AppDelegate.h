77b0f3cc4822ed07a5e6eca7e77277d2e657bf54
Smirnov Ivan <smirnovivan@macbook-pro-smirnov.local>
2016-09-23 00:22:11 +0300

    Initial Commit

---
+++ tests2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tests2
+//
+//  Created by Smirnov Ivan on 23.09.16.
+//  Copyright © 2016 Ivan Smirnov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tests2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tests2
+//
+//  Created by Smirnov Ivan on 23.09.16.
+//  Copyright © 2016 Ivan Smirnov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
